f06ea392b462c756dea46148c3b46e2c8c60cb7a
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-21 13:42:21 +0530

    added part time and full time employee wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,6 +2,40 @@ public class EmployeeWage
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Employee Wage Computation");
+		//constants
+		int IS_PART_TIME=1;
+		int IS_FULL_TIME=2;
+		int wagePerHour=20;
+		int wage=0;
+		int empHrs=0;
+	
+		//computation	
+		double empType=(int)Math.floor(Math.random()*10)%3;
+		if(empType==IS_FULL_TIME)
+		{
+			empHrs=16;
+			wage=empHrs*wagePerHour;
+			System.out.println("Full time employee");
+			System.out.println("Employee wage is "+wage);
+			
+			
+		}
+		else if(empType==IS_PART_TIME)
+		{
+			empHrs=8;
+			wage=empHrs*wagePerHour;
+			System.out.println("Part time employee");
+			System.out.println("Employee wage is "+wage);
+			
+			
+		}
+		else
+		{
+			wage=0;
+			System.out.println("Neither full time nor part time");
+			System.out.print("Wage is "+wage);
+		
+	
+	}
 	}
 }
