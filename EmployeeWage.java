dc7aa4f0ba41a14ae00390b3a739dc5699992112
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-21 14:30:30 +0530

    code added to calculate wage till end of month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,6 +2,43 @@ public class EmployeeWage
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Employee Wage Computation");
+     //variables
+		int wagePerHour=20;
+		int wage=0;
+		int empHrs=0;
+		int noOfdays=20;
+		int fullWage=0;
+		int partWage=0;
+		int totalWage=0;
+		
+		//iterating through 20 days using for loop
+		for(int i=0;i<noOfdays;i++)
+		{
+		//computation of employee type using switch case
+		int empType=(int)Math.floor(Math.random()*10)%3;
+		switch(empType)
+		{
+		case 2:empHrs=16;
+			   fullWage+=empHrs*wagePerHour*noOfdays;
+		       System.out.println("Full time employee");
+		       break;
+		               
+		case 1:empHrs=8;
+			   partWage+=empHrs*wagePerHour*noOfdays;
+			   System.out.println("Part time employee"); 
+			   break;
+		 default:wage=0;
+				System.out.println("Neither full time nor part time");
+				System.out.println("Wage is "+wage);
+	
+			
+			
+		}	
+
+	}
+		totalWage=fullWage+partWage;
+		System.out.println("Full time wage is "+fullWage);
+		System.out.println("Part time wage is "+partWage);
+		System.out.println("Total wage is "+totalWage);
 	}
 }
