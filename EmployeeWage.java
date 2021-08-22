29cc47db4602035928058d6ac0ae26978b471195
Anusha-Jagannath <67367819+Anusha-Jagannath@users.noreply.github.com>
2021-08-22 11:33:30 +0530

    added switch case statements

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,31 +2,29 @@ public class EmployeeWage
 {
 	public static void main(String[] args)
 	{
-		               //variables
-				int wagePerHour=20;
-				int wage=0;
-				int empHrs=0;
+		 //variables
+		int wagePerHour=20;
+		int wage=0;
+		int empHrs=0;
 				
-				//computation using switch case
-				int empType=(int)Math.floor(Math.random()*10)%3;
-				switch(empType)
-				{
-				case 2:empHrs=16;
-				       wage=empHrs*wagePerHour;
-				       System.out.println("Full time employee");
-				       System.out.println("Employee wage is "+wage);
-				       break;
-				case 1:empHrs=8;
-				       wage=empHrs*wagePerHour;
-				       System.out.println("Part time employee");
-				       System.out.println("Employee wage is "+wage);  
-				       break;
-				 default:wage=0;
-					System.out.println("Neither full time nor part time");
-					System.out.print("Wage is "+wage);
+		//computation using switch case
+		int empType=(int)Math.floor(Math.random()*10)%3;
+		switch(empType)
+		{
+		case 2:empHrs=16;
+		       wage=empHrs*wagePerHour;
+		       System.out.println("Full time employee");
+		       System.out.println("Employee wage is "+wage);
+		       break;
+		case 1:empHrs=8;
+		       wage=empHrs*wagePerHour;
+		       System.out.println("Part time employee");
+		       System.out.println("Employee wage is "+wage);  
+		       break;
+		default:wage=0;
+          		System.out.println("Neither full time nor part time");
+			System.out.print("Wage is "+wage);
 			
-					
-					
-				}	
+		}	
 	}
 }
