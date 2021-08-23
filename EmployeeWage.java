d9d11aadac4a402ab3a6a019d898192644dab834
Anusha-Jagannath <67367819+Anusha-Jagannath@users.noreply.github.com>
2021-08-23 11:02:49 +0530

    Update EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,44 +1,36 @@
 
 public class EmployeeWage
 {
+	final static int IS_PRESENT=1;
+        final static int WAGE_PER_HOUR=20;
+        final static int IS_PART_TIME=1;
+	final static int IS_FULL_TIME=2;
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Employee Wage Computation");
-	        //constants
+		//constants
 		int IS_PRESENT=1;
-		int wagePerHour=20;
-		int totalHour=8;
-		
-		//computation
+		int empWage=0;
+		int empHrs=0;
+		System.out.println("Welcome to Employee Wage Computation");
+	        //computation
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if(empCheck==IS_PRESENT)
+		{
+			empHrs=8;
 			System.out.println("Employee is present");
+		}
 		else
 			System.out.println("Employee is absent");
-		calculateEmployeeWage(wagePerHour,totalHour);
-	
-	}
-	//method to compute wage
-	public static void calculateEmployeeWage(int wagePerHour,int totalHour)
-	{
+		empWage=empHrs*WAGE_PER_HOUR;
+		System.out.println("Employee wage "+empWage);
 		
-		int wage=wagePerHour*totalHour;
-		System.out.println("Daily Employee Wage "+wage);
-	}
-
-		//constants
-		int IS_PART_TIME=1;
-		int IS_FULL_TIME=2;
-		int wagePerHour=20;
-		int wage=0;
-		int empHrs=0;
 	
 		//computation	
 		double empType=(int)Math.floor(Math.random()*10)%3;
 		if(empType==IS_FULL_TIME)
 		{
 			empHrs=16;
-			wage=empHrs*wagePerHour;
+			wage=empHrs*WAGE_PER_HOUR;
 			System.out.println("Full time employee");
 			System.out.println("Employee wage is "+wage);
 			
