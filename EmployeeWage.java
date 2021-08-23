cd33c2ecca14f4955224f0e43482912506232222
Anusha-Jagannath <67367819+Anusha-Jagannath@users.noreply.github.com>
2021-08-23 13:57:30 +0530

    Update EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,119 +1,108 @@
 
 public class EmployeeWage
 {
-	final static int IS_PRESENT=1;
-        final static int WAGE_PER_HOUR=20;
-        final static int IS_PART_TIME=1;
-	final static int IS_FULL_TIME=2;
+	//constants
+	public final static int IS_PRESENT=1;
+        public final static int WAGE_PER_HOUR=20;
+        public final static int IS_PART_TIME=1;
+	public final static int IS_FULL_TIME=2;
+	public final static int NO_OF_DAYS=20;
+	public final static int MAX_HRS_IN_MONTH=100; 
+	public final static int UM_OF_WORKING_DAYS
 	public static void main(String[] args)
 	{
-		//constants
-		int IS_PRESENT=1;
+		//variables
 		int empWage=0;
 		int empHrs=0;
+		int totalWage=0;
+		int totalWorkingDays=0;
+		int totalEmpHrs=0;
 		System.out.println("Welcome to Employee Wage Computation");
-	        
-	
-		//computation	
+		//computation
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck==IS_PRESENT)
+		{
+			empHrs=8;
+			System.out.println("Employee is present");
+		}
+		else
+			System.out.println("Employee is absent");
+		empWage=empHrs*WAGE_PER_HOUR;
+		System.out.println("Employee wage "+empWage);
+		
+	        //computation	
 		double empType=(int)Math.floor(Math.random()*10)%3;
 		if(empType==IS_FULL_TIME)
 		{
-			empHrs=16;
-			wage=empHrs*WAGE_PER_HOUR;
-			System.out.println("Full time employee");
-			System.out.println("Employee wage is "+wage);
-			
-			
+			empHrs=8;
+			System.out.println("Full time employee");				
 		}
 		else if(empType==IS_PART_TIME)
 		{
-			empHrs=8;
-			wage=empHrs*wagePerHour;
+			empHrs=4;
 			System.out.println("Part time employee");
-			System.out.println("Employee wage is "+wage);
-			
-			
 		}
-		else
-		{
-			wage=0;
-			System.out.println("Neither full time nor part time");
-			System.out.print("Wage is "+wage);
-		
-	
-	}
 
-		//variables
-		int wagePerHour=20;
-		int wage=0;
-		int empHrs=0;
-				
+		empWage=empHrs*WAGE_PER_HOUR;
+		System.out.println("Employee wage is "+empWage);
+		
+		
 		//computation using switch case
 		int empType=(int)Math.floor(Math.random()*10)%3;
 		switch(empType)
 		{
-		case 2:empHrs=16;
-	               wage=empHrs*wagePerHour;
-		       System.out.println("Full time employee");
-	               System.out.println("Employee wage is "+wage);
-		       break;
+		case IS_FULL_TIME:empHrs=8;
+		                  System.out.println("Full time employee");
+		                  break;
 				               
-		case 1:empHrs=8;
-		       wage=empHrs*wagePerHour;
-		       System.out.println("Part time employee");
-		       System.out.println("Employee wage is "+wage);  
-		       break;
-		 default:wage=0;
-			System.out.println("Neither full time nor part time");
-			System.out.print("Wage is "+wage);
-			
-		}	
-
-     //variables
-
-		int wagePerHour=20;
-		int wage=0;
-		int empHrs=0;
-		int noOfdays=20;
-
-		
-		//computation using switch case
-	        int fullWage=0;
-		int partWage=0;
-		int totalWage=0;
+		case IS_PART_TIME:empHrs=4;
+		                  System.out.println("Part time employee");
+		                  break;
+		 default:System.out.println("Employee is absent");	
+		}
+		empWage=empHrs*WAGE_PER_HOUR;
+		System.out.print("Wage is "+empWage);
 		
 		//iterating through 20 days using for loop
-		for(int i=0;i<noOfdays;i++)
+		for(int i=0;i<NO_OF_DAYS;i++)
 		{
-		//computation of employee type using switch case
-
 		int empType=(int)Math.floor(Math.random()*10)%3;
 		switch(empType)
 		{
-		case 2:empHrs=16;
-                       //wage=empHrs*wagePerHour*noOfdays;
-		       System.out.println("Full time employee");
-		       System.out.println("Employee wage is "+wage);
-		       fullWage+=empHrs*wagePerHour*noOfdays;
-		       break;
-	       case 1:empHrs=8;
-			partWage+=empHrs*wagePerHour*noOfdays;
-			System.out.println("Part time employee"); 
-			break;
-		default:wage=0;
-			System.out.println("Neither full time nor part time");
-			System.out.println("Wage is "+wage);
-
-			
-			
-		}	
+		case IS_FULL_TIME: empHrs=8;
+		       		   System.out.println("Full time employee");
+		                   break;
+	        case IS_PART_TIME:  empHrs=4;
+				   System.out.println("Part time employee"); 
+			           break;
+		default:empHrs=0;	
+		}
+		empWage=empHrs*WAGE_PER_HOUR
+	        totalWage+=empWage;
 
 
 	}
-		totalWage=fullWage+partWage;
-		System.out.println("Full time wage is "+fullWage);
-		System.out.println("Part time wage is "+partWage);
 		System.out.println("Total wage is "+totalWage);
+		
+
+		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
+		{
+			totalWorkingDays++;
+			int empType=(int)Math.floor(Math.random()*10)%3;
+			switch(empType)
+			{
+		 		case 2: empHrs=8;
+		       			System.out.println("Full time employee");
+		       			break;
+	         		case 1: empHrs=4;
+					System.out.println("Part time employee"); 
+					break;
+				default:empHrs=0;	
+			}
+		totalEmpHrs+=empHrs;
+		}
+		totalWage=totalEmpHrs*WAGE_PER_HOUR
+		System.out.println("Total Wage "+totalWage);
 
 	}
 }
